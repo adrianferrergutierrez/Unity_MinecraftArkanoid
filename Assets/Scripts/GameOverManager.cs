bee0c74515e475af1eb1ce7fc93e26ca7354a15d
francesc.perez.venegas <francesc.perez.venegas@estudiantat.upc.edu>
2025-05-25 21:11:06 +0200

    Cambios en el gameover

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -14,9 +14,25 @@ using UnityEngine.UI;
 public class GameOverManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public TextMeshProUGUI puntuacionFinalText; // Arrastra aquí tu TextMeshPro para la puntuación
+
+
     void Start()
     {
-        
+        // Ocultar el cursor o mostrarlo según necesites
+        // Cursor.lockState = CursorLockMode.None;
+        // Cursor.visible = true;
+
+        // --- MOSTRAR LA PUNTUACIÓN FINAL ---
+        if (puntuacionFinalText != null && GameManager.instance != null)
+        {
+            puntuacionFinalText.text = "Puntuación: " + GameManager.instance.puntuacion.ToString();
+        }
+        else if (puntuacionFinalText != null)
+        {
+            puntuacionFinalText.text = "Puntuación: N/A";
+        }
     }
 
     // Update is called once per frame
